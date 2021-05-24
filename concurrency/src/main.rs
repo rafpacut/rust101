4ae819ba870fff96acad53421506c64a4d597c87
rafal <rafpacut@gmail.com>
2021-05-24 09:02:05 +0200

    mutex demo

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,10 +1,12 @@
 use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
+use std::sync::{Arc, Mutex};
 
-fn main() {
+fn simple_communication() {
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = tx.clone();
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
@@ -12,6 +14,21 @@ fn main() {
             String::from("the"),
             String::from("other"),
             String::from("thread!"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
         ];
 
         for val in vals {
@@ -24,4 +41,29 @@ fn main() {
     for received in rx {
         println!("Got: {}", received);
     }
+}
+
+fn mutex_demo() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("num is: {}", counter.lock().unwrap());
+}
+
+fn main() {
+    mutex_demo();
 }
